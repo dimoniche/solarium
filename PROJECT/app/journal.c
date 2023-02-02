50e2f3b976d8543bf48836b56988228d9fe4af47
Dmitriy <dimoniche@gmail.com>
2023-02-02 23:20:16 +0300

    Очистка счетчика безналичной оплаты

--- PROJECT/app/journal.c
+++ PROJECT/app/journal.c
@@ -423,7 +423,7 @@ void CheckLongCounters(void)
 
 void ClearCounters(void)
 {
-  SetArrayFram(offsetof(TFramMap, Counters), sizeof(CPU_INT32U)*((CHANNELS_NUM)*4 + 3), 0x00);
+  SetArrayFram(offsetof(TFramMap, Counters), sizeof(CPU_INT32U)*((CHANNELS_NUM)*4 + 4), 0x00);
 }
 
 /// инкремент счетчика купюр по номиналам
--- PROJECT/version.h
+++ PROJECT/version.h
@@ -1,7 +1,7 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-#define   DEVICE_FW_VERSION   "03.40"
+#define   DEVICE_FW_VERSION   "03.41"
 
 
 #endif // #ifndef _VERSION_H_
